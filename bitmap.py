92710b0cc6115bb111dbb3c4d8a7b4bd96f3fe11
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2022-12-02 16:15:49 +0000

    Add files via upload

--- bitmap.py
+++ bitmap.py
@@ -6,6 +6,16 @@ def pset(images,x,y,r,g,b):
 	images[y][x][0]=r;
 	images[y][x][1]=g;
 	images[y][x][2]=b;
+def hline(images,x1,y1,x2,r,g,b):
+	xxx=range(x1,x2)
+	try:
+		for xx in xxx:
+			images[y1][xx][0]=r;
+			images[y1][xx][1]=g;
+			images[y1][xx][2]=b;
+	except:
+		return 1
+	return 0;
 n=0;
 nn=0;
 nnn=0;
@@ -15,8 +25,6 @@ print ("XXXX");
 image1=np.zeros((64,64,3),np.uint8);
 nn=range(0,63);
 for n in nn:
-	pset(image1,n,n,0,255,0);
-for n in nn:
-	pset(image1,n,63-n,0,255,0);
+	hline(image1,0,n,64,0,255,0)
 im=Image.fromarray(image1);
 im.save("my.jpeg");
